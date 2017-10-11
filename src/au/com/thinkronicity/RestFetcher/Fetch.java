452438183ff5f0377f9d4102d51a7716a61da62a
Ian Hogan <Ian_MacDonald_Hogan@yahoo.com>
2017-10-11 07:55:31 +0000

    Updated version to 2.4.9CE

--- src/au/com/thinkronicity/RestFetcher/Fetch.java
+++ src/au/com/thinkronicity/RestFetcher/Fetch.java
@@ -50,7 +50,7 @@ implements RequestHandler<Object, String> {
     /**
      * Version of this codebase.
      */
-    private static final String version = "2.4.8CE";
+    private static final String version = "2.4.9CE";
     
     /**
      * Namespace for the Commands XML schema. 
